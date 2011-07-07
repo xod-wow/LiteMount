297cf00382913143c0da7d57d5096327cb59211a
Mike "Xodiv" Battersby <mib@post.com>
2011-07-07 10:38:09 +0000

    Stupid buttons :(

--- UIOptionsFrame.lua
+++ UIOptionsFrame.lua
@@ -13,9 +13,10 @@ local MIN_GAP = 5
 function LiteMountOptions_CreateButton(panel, i)
     local b = CreateFrame("Button", panel:GetName().."Button"..i, panel, "LiteMountOptionsButtonTemplate")
     -- Dummy values for testing
-    local n,_,i = GetSpellInfo(48025)
-    b.name:SetText(n)
-    b.icon:SetTexture(i)
+    local name,_,icon = GetSpellInfo(48025)
+    b.name:SetText(name)
+    b.icon:SetTexture(icon)
+    return b
 end
 
 function LiteMountOptions_CreateButtons(self)
@@ -30,21 +31,24 @@ function LiteMountOptions_CreateButtons(self)
     local totalheight = panelheight - 2*PANEL_MARGIN
     local buttonwidth, buttonheight = self.buttons[1]:GetSize()
 
+    buttonheight = 45
+
     local rows = floor((totalwidth + MIN_GAP) / (buttonwidth + MIN_GAP))
     local cols = floor((totalheight + MIN_GAP) / (buttonheight + MIN_GAP))
 
     local hgap = floor((totalwidth - rows*buttonwidth)/(rows-1))
     local vgap = floor((totalheight - cols*buttonheight)/(cols-1))
 
-    for i = 1, cols do
-        for j = 1, rows do
-            local b = i * cols + j
+    for i = 1, rows do
+        for j = 1, cols do
+            local b = (i-1) * cols + j
+LM_Print("Creating button " .. b)
             if not self.buttons[b] then
                 self.buttons[b] = LiteMountOptions_CreateButton(self, b)
             end
-            local y = -(PANEL_MARGIN + (i-1)*(buttonheight+vgap))
-            local x = PANEL_MARGIN + (j-1)*(buttonwidth+hgap)
-            self.buttons[i]:SetPoint("TOPLEFT", nil, "TOPLEFT", x, y)
+            local y = -(PANEL_MARGIN + (j-1)*(buttonheight+vgap))
+            local x = PANEL_MARGIN + (i-1)*(buttonwidth+hgap)
+            self.buttons[b]:SetPoint("TOPLEFT", self, "TOPLEFT", x, y)
         end
     end
     self.numButtons = #self.buttons
@@ -59,7 +63,7 @@ function LiteMountOptions_OnLoad(self)
 
     self.title:SetText(self.name)
 
-    LiteMountOptions_CreateButtons(self)
+    -- LiteMountOptions_CreateButtons(self.container)
 
     InterfaceOptions_AddCategory(self)
 end
