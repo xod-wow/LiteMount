ba31774b6a0eaf8fcaa09fac3b515162b4e97dec
Mike "Xodiv" Battersby <mib@post.com>
2011-09-05 13:37:13 +0000

    More flags override stuff in options.

--- Options.lua
+++ Options.lua
@@ -72,7 +72,7 @@ end
    Mount flag overrides stuff
 ----------------------------------------------------------------------------]]--
 
-function LM_Options:SaveMountFlags(mount)
+function LM_Options:SaveMountFlags(mount, flags)
     local id = mount:SpellId()
 
     if not self.flagoverrides[id] then
@@ -82,46 +82,49 @@ function LM_Options:SaveMountFlags(mount)
     local def = mount:GetDefaultFlags()
     local cur = mount:GetFlags()
 
-    -- XXX FIXME XXX
+    local toset = bit.band(bit.bxor(def, cur), cur)
+    local toclear = bit.band(bit.bxor(def, cur), bit.bnot(cur))
+
+    self.flagoverrides[id][1] = toset
+    self.flagoverrides[id][2] = toclear
 end
 
-function LM_Options:ApplySavedFlags(mount)
+function LM_Options:ModFlags(mount)
     local id = mount:SpellId()
+    local flags = mount:DefaultFlags()
 
     local ov = self.flagoverrides[id]
 
-    if not ov then return end
 
-    local flags = mount:DefaultFlags()
+    if not ov then return flags end
 
     flags = bit.bor(flags, ov[1])
     flags = bit.band(flags, bit.bnot(ov[1]))
 
-    -- XXX FIXME XXX
-    -- mount:SetFlags(flags)
+    return flags
 end
 
 function LM_Options:SetMountFlagBit(mount, flag)
     LM_Debug(string.format("Setting flag bit %d for spell %s (%d).",
                            flag, GetSpellInfo(id), id))
 
-    mount:SetFlags(bit.bor(mount:Flags(), flag))
-    LM_Options:SaveMountFlags(mount)
+    local newflags = bit.bor(mount:Flags(), flag)
+    LM_Options:SaveMountFlags(mount, newflags)
 end
 
 function LM_Options:ClearMountFlagBit(mount, flag)
     LM_Debug(string.format("Clearing flag bit %d for spell %s (%d).",
                            flag, GetSpellInfo(id), id))
 
-    mount:SetFlags(bit.band(mount:Flags(), bit.bnot(flag)))
-    LM_Options:SaveMountFlags(mount)
+    local newflags = bit.band(mount:Flags(), bit.bnot(flag))
+    LM_Options:SaveMountFlags(mount, newflags)
 end
 
 function LM_Options:ResetMountFlags(mount)
     LM_Debug(string.format("Defaulting flag bit %d for spell %s (%d).",
                            flag, GetSpellInfo(id), id))
 
-    mount:SetFlags(mount:DefaultFlags())
-    LM_Options:SaveMountFlags(mount)
+    local newflags = mount:DefaultFlags()
+    LM_Options:SaveMountFlags(mount, newflags)
 end
 
