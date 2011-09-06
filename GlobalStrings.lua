e96bfe3c69dca91b8717baf666684c499bfc205f
Mike "Xodiv" Battersby <mib@post.com>
2011-09-06 07:49:40 +0000

    Remove unused GlobalStrings.lua

--- GlobalStrings.lua
+++
@@ -1,9 +0,0 @@
---[[----------------------------------------------------------------------------
-
-  LiteMount/GlobalSrings.lua
-
-  Localization.
-
-----------------------------------------------------------------------------]]--
-
-LM_ERR_NO_MOUNT_USABLE = "You don't know any mounts you can use here."
