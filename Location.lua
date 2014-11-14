77959913f37be570a75ba022fd2d29417becc896
Mike "Xodiv" Battersby <mib@unimelb.edu.au>
2014-11-14 11:47:01 +1100

    Fix bug with lua != being ~=

--- Location.lua
+++ Location.lua
@@ -74,7 +74,7 @@ end
 -- Draenor (continent 7) is flagged flyable even though you can't fly there
 -- Maybe this will be fixed in 6.1. Is there no end to the horror?
 function LM_Location:CanFly()
-    if self.continent != 7 then
+    if self.continent ~= 7 then
         return IsFlyableArea()
     else
         return nil
