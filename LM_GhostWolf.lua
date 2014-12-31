abf25b4659b99df1840ecfdc4ad82d77dc1f6b02
Mike "Xodiv" Battersby <mib@post.com>
2014-12-31 15:14:16 +1100

    Tablet of Ghost Wolf makes Ghost Wolf 100% mount in Ashran.

--- LM_GhostWolf.lua
+++ LM_GhostWolf.lua
@@ -6,11 +6,22 @@
 
 ----------------------------------------------------------------------------]]--
 
+local LM_SPELL_TABLET_OF_GHOST_WOLF = 168799
+
 LM_GhostWolf = setmetatable({ }, LM_Spell)
 LM_GhostWolf.__index = LM_GhostWolf
 
+local tabletBuffName
+
 function LM_GhostWolf:DefaultFlags(v)
-    return LM_FLAG_BIT_WALK
+    if not tabletBuffName then
+        tabletBuffName = GetSpellInfo(LM_SPELL_TABLET_OF_GHOST_WOLF)
+    end
+    if UnitAura("player", tabletBuffName) then
+        return LM_FLAG_BIT_RUN
+    else
+        return LM_FLAG_BIT_WALK
+    end
 end
 
 function LM_GhostWolf:Get()
