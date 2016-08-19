74958c40510d6441e640cc5713095b7b6e652aaa
Mike "Xodiv" Battersby <mib@post.com>
2016-08-20 09:48:17 +1000

    Rename the options lua files to be shorter

---
+++ UI/ActionLists.lua
@@ -0,0 +1,16 @@
+--[[----------------------------------------------------------------------------
+
+  LiteMount/UIOptionsActionLists.lua
+
+  Options frame for the action list.
+
+  Copyright 2011-2016 Mike Battersby
+
+----------------------------------------------------------------------------]]--
+
+function LiteMountOptionsMounts_OnLoad(self)
+
+    self.name = "Action List"
+
+    LiteMountOptionsPanel_OnLoad(self)
+end
