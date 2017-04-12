68a8ff93294bf1905d427da33ea49ab95ca74108
Mike "Xodiv" Battersby <mib@post.com>
2017-04-12 14:53:50 +1000

    Get rid of the scripted /run for the old falcosaur support.

--- LM_Journal.lua
+++ LM_Journal.lua
@@ -105,26 +105,6 @@ function LM_Journal:IsUsable()
     return LM_Mount.IsUsable(self)
 end
 
--- New mounts can't be summoned by casting their spell any more. :(
--- Annoyingly when the mount journal UI calls SummonByID it cancels forms
--- that would prevent mounting. When we call it, it doesn't. :( :(
-
-function LM_Journal:SetupActionButton(button)
-    local t = ""
-
-    if select(2, UnitClass("player")) ==  "DRUID" then
-        t = t .. "/cancelform [form:1/2]\n"
-    end
-
-    t = t .. format("/run C_MountJournal.SummonByID(%d)", self:MountID())
-    button:SetAttribute("type", "macro")
-    button:SetAttribute( "macrotext", t)
-end
-
---[[
-function LM_Journal:SetupActionButton(button)
-    MountJournal.selectedMountID = self.mountID
-    button:SetAttribute("type", "click")
-    button:SetAttribute("clickbutton", MountJournalMountButton)
-end
-]]
+-- Note, at some point we needed to use type=macro and
+-- /run C_MountJournal.SummonByID(%d)
+-- in a custom SetupActionButton, but now don't again, phew.
