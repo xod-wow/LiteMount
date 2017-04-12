64b814ca754a81fd85d868e00e9933d2a391fa6a
Mike "Xodiv" Battersby <mib@post.com>
2017-04-12 13:11:37 +1000

    Apparently one of the Order Hall followers makes Ghost Wolf 100%.

--- LM_GhostWolf.lua
+++ LM_GhostWolf.lua
@@ -6,18 +6,16 @@
 
 ----------------------------------------------------------------------------]]--
 
-local LM_SPELL_TABLET_OF_GHOST_WOLF = 168799
+local TABLET_OF_GHOST_WOLF_AURA = GetSpellInfo(168799)
+local SPIRIT_PACK_AURA = GetSpellInfo(217850)
 
 LM_GhostWolf = setmetatable({ }, LM_Spell)
 LM_GhostWolf.__index = LM_GhostWolf
 
-local tabletBuffName
-
 function LM_GhostWolf:Flags(v)
-    if not tabletBuffName then
-        tabletBuffName = GetSpellInfo(LM_SPELL_TABLET_OF_GHOST_WOLF)
-    end
-    if UnitAura("player", tabletBuffName) then
+    if UnitAura("player", TABLET_OF_GHOST_WOLF_AURA) then
+        return LM_FLAG_BIT_RUN
+    elseif UnitAura("player", SPIRIT_PACK_AURA) then
         return LM_FLAG_BIT_RUN
     else
         return LM_FLAG_BIT_WALK
