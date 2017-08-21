4b35b5604ca432f6e6d99cf246920c7ea86ba49f
Mike "Xodiv" Battersby <mib@post.com>
2017-08-21 19:42:26 +1000

    Generic keybinding names now we have advanced.

--- KeyBindingStrings.lua
+++ KeyBindingStrings.lua
@@ -11,7 +11,7 @@
 local L = LM_Localize
 
 BINDING_HEADER_LITEMOUNT_TITLE = GetAddOnMetadata("LiteMount", "Title")
-_G["BINDING_NAME_CLICK LM_B1:LeftButton"] = MOUNT .. " / " .. BINDING_NAME_DISMOUNT
-_G["BINDING_NAME_CLICK LM_B2:LeftButton"] = L["Non-flying Mount"] .. " / " .. BINDING_NAME_DISMOUNT
-_G["BINDING_NAME_CLICK LM_B3:LeftButton"] = format("%s %s 1 / %s", MOUNT, CUSTOM, BINDING_NAME_DISMOUNT)
-_G["BINDING_NAME_CLICK LM_B4:LeftButton"] = format("%s %s 2 / %s", MOUNT, CUSTOM, BINDING_NAME_DISMOUNT)
+_G["BINDING_NAME_CLICK LM_B1:LeftButton"] = format('%s %d', KEY_BINDING, 1)
+_G["BINDING_NAME_CLICK LM_B2:LeftButton"] = format('%s %d', KEY_BINDING, 2)
+_G["BINDING_NAME_CLICK LM_B3:LeftButton"] = format('%s %s', KEY_BINDING, 3)
+_G["BINDING_NAME_CLICK LM_B4:LeftButton"] = format('%s %s', KEY_BINDING, 4)
