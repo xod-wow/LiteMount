464856546af2a72b66363cc0c9302c172d04f0bd
Mike "Xodiv" Battersby <mib@post.com>
2017-08-29 16:29:05 +1000

    Back to one advanced panel.

--- UI/AdvancedActions.lua
+++ UI/AdvancedActions.lua
@@ -14,6 +14,43 @@ local function BindingText(n)
     return format('%s %s', KEY_BINDING, n)
 end
 
+function LiteMountOptionsFlagList_Update(self)
+    local scrollFrame = self.ScrollFrame
+    local offset = HybridScrollFrame_GetOffset(scrollFrame)
+    local buttons = scrollFrame.buttons
+
+    local allFlags = LM_Options:GetAllFlags()
+
+    local totalHeight = #allFlags * buttons[1]:GetHeight()
+    local displayedHeight = #buttons * buttons[1]:GetHeight()
+
+    for i = 1, #buttons do
+        button = buttons[i]
+        index = offset + i
+        if index <= #allFlags then
+            local flagText = allFlags[index]
+            if LM_Options:IsPrimaryFlag(allFlags[index]) then
+                flagText = ITEM_QUALITY_COLORS[5].hex .. flagText .. FONT_COLOR_CODE_CLOSE
+            end
+            button.Text:SetFormattedText(flagText)
+            button:Show()
+            button.flag = allFlags[index]
+        else
+            button:Hide()
+            button.flag = nil
+        end
+    end
+
+    HybridScrollFrame_Update(scrollFrame, totalHeight, displayedHeight)
+end
+
+function LiteMountOptionsActions_OnSizeChanged(self)
+    HybridScrollFrame_CreateButtons(self.ScrollFrame, "LiteMountOptionsFlagButtonTemplate", 0, 0, "TOPLEFT", "TOPLEFT", 0, 0, "TOP", "BOTTOM")
+    for _,b in ipairs(self.ScrollFrame.buttons) do
+        b:SetWidth(self:GetWidth())
+    end
+end
+
 function LiteMountOptionsActions_OnLoad(self)
     self.name = format('%s : %s', ADVANCED_LABEL, L.LM_ACTION_LISTS)
 
@@ -21,6 +58,10 @@ function LiteMountOptionsActions_OnLoad(self)
 
     UIDropDownMenu_Initialize(self.BindingDropDown, LiteMountOptionsActionsBindingDropDown_Initialize)
     UIDropDownMenu_SetText(self.BindingDropDown, BindingText(1))
+
+    self.ScrollFrame.update = function () LiteMountOptionsFlagList_Update(self) end
+    LiteMountOptionsActions_OnSizeChanged(self)
+
     LiteMountOptionsPanel_OnLoad(self)
 end
 
@@ -28,6 +69,7 @@ function LiteMountOptionsActions_OnShow(self)
     self.EditBox:Disable()
     self.EditBox:SetAlpha(0.5)
     self.UnlockButton:Show()
+    LiteMountOptionsFlagList_Update(self)
     LiteMountOptionsPanel_OnShow(self)
 end
 
