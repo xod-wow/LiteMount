d7230a538e5f34790b60cca9fe7bda8153344c3d
Mike "Xodiv" Battersby <mib@post.com>
2018-04-05 07:03:43 +1000

    Hard-coded list of shapeshift forms to restore

--- Actions.lua
+++ Actions.lua
@@ -55,15 +55,25 @@ ACTIONS['Dismount'] =
     end
 
 -- Only cancel forms that we will activate (mount-style ones).
--- See: http://wowprogramming.com/docs/api/GetShapeshiftFormID
+-- See: https://wow.gamepedia.com/API_GetShapeshiftFormID
+-- Form IDs that you put here must be cancelled automatically on
+-- mounting and dismount you when cast.
 
 local prevFormName
+local restoreFormIDs = {
+    [1] = true,     -- Cat Form
+    [5] = true,     -- Bear Form
+    [16] = true,    -- Ghost Wolf
+    [30] = true,    -- Rogue Stealth
+    [31] = true,    -- Moonkin Form
+}
 
 ACTIONS['CancelForm'] =
     function ()
         LM_Debug("Trying CancelForm")
 
         local curFormIndex = GetShapeshiftForm()
+        local curFormID = GetShapeshiftFormID()
         local inMountForm = curFormIndex > 0 and LM_PlayerMounts:GetMountByShapeshiftForm(curFormIndex)
 
         LM_Debug("Previous form is " .. tostring(prevFormName))
@@ -73,7 +83,7 @@ ACTIONS['CancelForm'] =
                 LM_Debug("Setting action to " .. prevFormName)
                 return LM_SecureAction:Spell(prevFormName)
             end
-        elseif curFormIndex and curFormIndex > 0 then
+        elseif curFormID and restoreFormIDs[curFormID] then
             local _, name = GetShapeshiftFormInfo(curFormIndex)
             LM_Debug("Saving current form " .. tostring(name) .. ".")
             prevFormName = name
@@ -203,7 +213,7 @@ function LM_Actions:DefaultCombatMacro()
 
     local mt = "/dismount [mounted]\n"
 
-    local playerClass = select(2, UnitClass("player"))
+    local _, playerClass = UnitClass("player")
 
     if playerClass ==  "DRUID" then
         local forms = GetDruidMountForms()
