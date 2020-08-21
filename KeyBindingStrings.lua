f4ee88812cdd4b474f3f1ffbd304ecc8394b434d
Mike "Xodiv" Battersby <mib@post.com>
2020-08-21 23:45:25 +1000

    Add LiteMount to the key binding names

--- KeyBindingStrings.lua
+++ KeyBindingStrings.lua
@@ -9,7 +9,7 @@
 ----------------------------------------------------------------------------]]--
 
 BINDING_HEADER_LITEMOUNT_TITLE = GetAddOnMetadata("LiteMount", "Title")
-_G["BINDING_NAME_CLICK LM_B1:LeftButton"] = format('%s %d', KEY_BINDING, 1)
-_G["BINDING_NAME_CLICK LM_B2:LeftButton"] = format('%s %d', KEY_BINDING, 2)
-_G["BINDING_NAME_CLICK LM_B3:LeftButton"] = format('%s %s', KEY_BINDING, 3)
-_G["BINDING_NAME_CLICK LM_B4:LeftButton"] = format('%s %s', KEY_BINDING, 4)
+_G["BINDING_NAME_CLICK LM_B1:LeftButton"] = format('LiteMount %s %d', KEY_BINDING, 1)
+_G["BINDING_NAME_CLICK LM_B2:LeftButton"] = format('LiteMount %s %d', KEY_BINDING, 2)
+_G["BINDING_NAME_CLICK LM_B3:LeftButton"] = format('LiteMount %s %s', KEY_BINDING, 3)
+_G["BINDING_NAME_CLICK LM_B4:LeftButton"] = format('LiteMount %s %s', KEY_BINDING, 4)
