6306375e16cfa1381f2824c913f2f581189d9d64
Mike "Xodiv" Battersby <mib@post.com>
2020-12-29 10:57:20 +1100

    Try to restrict Running Wild to when you are in proper form

--- LM_RunningWild.lua
+++ LM_RunningWild.lua
@@ -18,3 +18,17 @@ LM.RunningWild.__index = LM.RunningWild
 function LM.RunningWild:Get()
     return LM.Spell.Get(self, LM.SPELL.RUNNING_WILD, 'RUN')
 end
+
+local worgenPlayerModels = {
+    [ 307453] = true,       -- Worgen male
+    [ 307454] = true,       -- Worgen female
+    [1011653] = true,       -- Human male
+    [1000764] = true,       -- Human female
+}
+
+-- Running Wild doesn't work if you're shapeshifted in any way.
+
+function LM.RunningWild:IsCastable()
+    local id = LM.Environment:GetPlayerModel()
+    return worgenPlayerModels[id] and LM.Spell.IsCastable(self)
+end
