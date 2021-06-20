76b056a6071839da1405df88e600c68dfc7f94d0
Mike "Xodiv" Battersby <mib@post.com>
2021-06-20 23:30:24 +1000

    Fix defaults for rules

--- UI/Rules.lua
+++ UI/Rules.lua
@@ -92,7 +92,7 @@ function LiteMountRulesScrollMixin:GetOption(i)
 end
 
 function LiteMountRulesScrollMixin:GetOptionDefault()
-    return LM.Options:GetRules('*')
+    return CopyTable(LM.Options:GetRules('*'))
 end
 
 function LiteMountRulesScrollMixin:OnLoad()
