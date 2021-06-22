10cc7fcc1bcc36f5441baeec7eb99975294763e6
Mike "Xodiv" Battersby <mib@post.com>
2021-06-22 17:59:17 +1000

    Clear selectedRule on defaults apply

--- UI/Rules.lua
+++ UI/Rules.lua
@@ -84,6 +84,7 @@ function LiteMountRulesScrollMixin:Update()
 end
 
 function LiteMountRulesScrollMixin:SetOption(v, i)
+    self:GetParent().selectedRule = nil
     return LM.Options:SetRules(i, v)
 end
 
