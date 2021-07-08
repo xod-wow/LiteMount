d25ea9efad05ccc1451ca06887cd7f35533929a0
Mike Battersby <mib@post.com>
2021-07-08 15:43:53 +1000

    Start on priority import maybe

---
+++ UI/PriorityImport.lua
@@ -0,0 +1,30 @@
+--[[----------------------------------------------------------------------------
+
+  LiteMount/UI/PriorityImport.lua
+
+  Pop-over to allow import of priorities via cut-and-paste.
+
+  Copyright 2011-2021 Mike Battersby
+
+----------------------------------------------------------------------------]]--
+
+local _, LM = ...
+
+local L = LM.Localize
+
+--[[
+/run LiteMountOptionsPanel_PopOver(LiteMountMountsPanel, LiteMountPriorityImport)
+]]
+
+LiteMountPriorityImportMixin = {}
+
+function LiteMountPriorityImportMixin:Okay()
+    local text = self.Scroll.EditBox:GetText()
+    for line in text:gmatch('[^\r\n]+') do
+    end
+    -- self:Hide()
+end
+
+function LiteMountPriorityImportMixin:Cancel()
+    self:Hide()
+end
