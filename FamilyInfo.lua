547c4ce9bf501d33551cdbdb4b80a0bee7e25374
Pol <snamor@users.noreply.github.com>
2021-08-27 19:03:02 +0200

    Add Mastercraft Gravewing mount
    This mount had a bug which prevented it from appearing in the journal. They seem to thave fixed it, but now the addon breaks so this should fix it. More info on the bug: https://www.wowhead.com/item=186479/mastercraft-gravewing#comments

--- FamilyInfo.lua
+++ FamilyInfo.lua
@@ -501,6 +501,7 @@ LM.MOUNTFAMILY["Gravewing"] = {
     [353866] = true, -- Obsidian Gravewing
     [353873] = true, -- Pale Gravewing
     [353872] = true, -- Sinfall Gravewing
+    [215545] = true, -- Mastercraft Gravewing
 }
 
 LM.MOUNTFAMILY["Gronnling"] = {
