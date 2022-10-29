3b71f5204083ef7ec94a9c637bde88339d6efc94
Mike "Xodiv" Battersby <mib@post.com>
2022-10-29 20:26:59 +1100

    Profile button tweaks

--- UI/Profiles.lua
+++ UI/Profiles.lua
@@ -16,13 +16,20 @@ local LibDD = LibStub("LibUIDropDownMenu-4.0")
 
 LiteMountProfilesPanelMixin = {}
 
-L.LM_PROFILES_EXP = "Here you can manage profiles, different LiteMount configurations that you can switch between. Each character has its own selected profile.\n\nSorry about this low-effort page, this is a quick hack for 10.0 / Dragonflight. It will improve soon.\n\nUse the drop down menu below to manage profiles."
+L.LM_PROFILES_EXP = [[Here you can manage profiles, different LiteMount configurations that you can switch between. Each character has its own selected profile.
+
+Sorry about this low-effort page, this is a quick hack for 10.0 / Dragonflight. It will improve soon.
+
+Use the drop down menu below to manage profiles.]]
 
 function LiteMountProfilesPanelMixin:OnLoad()
 
     self.name = L.LM_PROFILES
 
-    LiteMountProfileButton:Attach(self)
+    LiteMountProfileButton:SetParent(self)
+    LiteMountProfileButton:ClearAllPoints()
+    LiteMountProfileButton:SetPoint("TOPLEFT", self, "TOPLEFT", 49, -200)
+    LiteMountProfileButton:Show()
 
     -- LibDD:Create_UIDropDownMenu(self.RandomPersistDropDown)
 
