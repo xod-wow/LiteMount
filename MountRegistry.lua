1d5c9790855ed9066f5c01f29ff065e5c06b8eb4
Mike "Xodiv" Battersby <mib@post.com>
2024-07-27 13:48:19 +1000

    Mark Soar for steady flight. Fixes #266

--- MountRegistry.lua
+++ MountRegistry.lua
@@ -26,7 +26,7 @@ local MOUNT_SPELLS = {
     { "GhostWolf", LM.SPELL.GHOST_WOLF, 'RUN', 'SLOW' },
     { "Nagrand", LM.SPELL.FROSTWOLF_WAR_WOLF, 'Horde', 'RUN' },
     { "Nagrand", LM.SPELL.TELAARI_TALBUK, 'Alliance', 'RUN' },
-    { "Soar", LM.SPELL.SOAR, 'DRAGONRIDING' },
+    { "Soar", LM.SPELL.SOAR, 'FLY', 'DRAGONRIDING' },
 --  { "Soulshape", LM.SPELL.SOULSHAPE, 'RUN', 'SLOW' },
     { "ItemSummoned",
         LM.ITEM.MAGIC_BROOM, LM.SPELL.MAGIC_BROOM, 'RUN', 'FLY', },
