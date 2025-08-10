4e023a9455f32e19d645bbb96400a379154a6eaf
Mike "Xodiv" Battersby <mib@post.com>
2025-08-10 19:36:13 +1000

    Oops accidentally committed part of a future change
    Fixes #359

--- UI/MountModelSceneTemplate.lua
+++ UI/MountModelSceneTemplate.lua
@@ -15,10 +15,9 @@ function LiteMountMountModelSceneMixin:SetMount(mount)
         self:TransitionToModelSceneID(mount.modelSceneID, CAMERA_TRANSITION_TYPE_IMMEDIATE, CAMERA_MODIFICATION_TYPE_DISCARD, false)
         local mountActor = self:GetActorByTag("unwrapped")
         if mountActor then
-            local n = math.random(#mount.creatureDisplayID)
             mountActor:Hide()
             mountActor:SetOnModelLoadedCallback(function () mountActor:Show() end)
-            mountActor:SetModelByCreatureDisplayID(mount.creatureDisplayID[n], true)
+            mountActor:SetModelByCreatureDisplayID(mount.creatureDisplayID, true)
             if mount.isSelfMount then
                 mountActor:SetAnimationBlendOperation(Enum.ModelBlendOperation.None)
                 mountActor:SetAnimation(618)
