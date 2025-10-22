dc6c488b8fc12666e67b95d5d8ed533e3f4ea635
Mike "Xodiv" Battersby <mib@post.com>
2025-10-22 11:23:51 +1100

    Darken the list item style and prune some templates

--- UI/PanelTemplate.lua
+++ UI/PanelTemplate.lua
@@ -64,17 +64,7 @@ LM_POPOVER_BACKDROP_INFO = {
     insets = { left = 3, right = 3, top = 3, bottom = 3 },
 }
 
-LM_BUTTON_BACKDROP_INFO = {
-    bgFile = "Interface/Tooltips/UI-Tooltip-Background",
-    edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
-    tile = true,
-    tileEdge = true,
-    tileSize = 8,
-    edgeSize = 8,
-    insets = { left = 4, right = 4, top = 4, bottom = 4 },
-}
-
-LM_LISTBUTTON_BACKDROP_INFO = {
+LM_LIST_ITEM_BACKDROP_INFO = {
     bgFile = "Interface/Buttons/UI-SliderBar-Background",
     edgeFile = "Interface/Buttons/UI-SliderBar-Border",
     tile = true,
